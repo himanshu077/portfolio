dca1403b575a1f82c20d4ca0475eb4741567f94e
Himanshu Kumar <himanshukumar077@gmail.com>
2024-02-22 10:39:46 +0530

    Added images and started about page

---
+++ src/components/sections/About/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import backgroundImage from "../../../assets/Images/background.webp";
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
